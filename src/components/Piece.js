e7b8b66d1430dd771d75202fb127690c02ee7e1a
Daniel Valldosera Moreno <dani@dimo.cat>
2018-12-31 18:35:07 +0100

    changed logic of translate by drag Component

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Draggable from 'react-draggable';
 import './piece-style.css';
 
 class Piece extends Component{
 
+	handleStart(){
+		console.log('start');
+	}
+
+	handleDrag(){
+
+	}
+
+	handleStop(){
+		console.log('stop');
+	}
+
 	place(){
 		return{
-			'transform': `translate3d(${this.props.col*this.props.size}px,${this.props.row*this.props.size}px,1px)`,
+			//'transform': `translate3d(${this.props.col*this.props.size}px,${this.props.row*this.props.size}px,1px)`,
 			'width': this.props.size,
 			'height': this.props.size,
 			'backgroundImage': `url('${this.props.img}')`,
@@ -16,7 +29,16 @@ class Piece extends Component{
 
 	render(){
 		return (
-			<div id={`${this.props.id}`} className="piece" style={this.place()}></div>
+			<Draggable
+				onStart={this.handleStart.bind(this)}
+    			onDrag={this.handleDrag.bind(this)}
+				onStop={this.handleStop.bind(this)}
+				axis="x"
+				defaultPosition={{x: this.props.col*this.props.size, y: this.props.row*this.props.size}}
+				position={{x: this.props.col*this.props.size, y: this.props.row*this.props.size}}
+			>
+				<div id={`${this.props.id}`} className="piece" style={this.place()}></div>
+			</Draggable>
 		);
 	}
 }
