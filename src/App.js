f2f77d218bb64b5a29db1c05ff432012320b8702
Daniel Valldosera Moreno <dani@dimo.cat>
2019-01-01 12:45:20 +0100

    spaces

--- src/App.js
+++ src/App.js
@@ -6,46 +6,46 @@ import gifImg from './imgs/30db44484b1e5d0e2f2531b1005e1449c18b141d_00.gif';
 class App extends Component {
 
   constructor(props){
-    super(props);
-    this.maxSize = 300;
-    this.state = {
-      sizeGame : (window.innerWidth-200 > this.maxSize)?this.maxSize:window.innerWidth-200
-    }
+	super(props);
+	this.maxSize = 300;
+	this.state = {
+	  sizeGame : (window.innerWidth-200 > this.maxSize)?this.maxSize:window.innerWidth-200
+	}
   }
 
   componentDidMount(){
-    window.addEventListener('resize', this.handleOnResize.bind(this))
+	window.addEventListener('resize', this.handleOnResize.bind(this))
   }
 
   handleClick(){
-    this.refs.gameRef.shuffle();
+	this.refs.gameRef.shuffle();
   }
 
   handleOnResize(){
-    this.setState(()=>{
-      return{
-        sizeGame : (window.innerWidth-200 > this.maxSize)?this.maxSize:window.innerWidth-200
-      }
-    })
+	this.setState(()=>{
+	  return{
+		sizeGame : (window.innerWidth-200 > this.maxSize)?this.maxSize:window.innerWidth-200
+	  }
+	})
   }
 
   render() {
-    return (
-      <div className="App">
-        <Game
-          ref="gameRef"
-        	grid={5} 
-        	size={this.state.sizeGame} 
-        	img={img} 
-        	onWin={()=>{alert('YOU WON!')}}
-        />
-        <p>
-          <button onClick={this.handleClick.bind(this)}>
-            SHUFFLE
-          </button>
-        </p>
-      </div>
-    );
+	return (
+	  <div className="App">
+		<Game
+		  ref="gameRef"
+			grid={5}
+			size={this.state.sizeGame}
+			img={img}
+			onWin={()=>{alert('YOU WON!')}}
+		/>
+		<p>
+		  <button onClick={this.handleClick.bind(this)}>
+			SHUFFLE
+		  </button>
+		</p>
+	  </div>
+	);
   }
 }
 
