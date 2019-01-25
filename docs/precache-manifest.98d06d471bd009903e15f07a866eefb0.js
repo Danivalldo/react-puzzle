f665692c86af2e1fb3c400ee741ac7382ff52023
Daniel Valldosera Moreno <dani@dimo.cat>
2019-01-25 14:27:08 +0100

    added docs

---
+++ docs/precache-manifest.98d06d471bd009903e15f07a866eefb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "727a4e2d7c0a862eef5da6508722cceb",
+    "url": "./static/media/713f36b9c2e2aca894b2290d8cb788d9.727a4e2d.jpg"
+  },
+  {
+    "revision": "6613ee17c66c15d02520f85252dec2a2",
+    "url": "./static/media/30db44484b1e5d0e2f2531b1005e1449c18b141d_00.6613ee17.gif"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "bed9a46bc5f30c1e313a",
+    "url": "./static/js/main.bed9a46b.chunk.js"
+  },
+  {
+    "revision": "ae1334b2f6b3b721806d",
+    "url": "./static/js/1.ae1334b2.chunk.js"
+  },
+  {
+    "revision": "bed9a46bc5f30c1e313a",
+    "url": "./static/css/main.161cf82a.chunk.css"
+  },
+  {
+    "revision": "2dae6b2ddbc40fc23ca7eb4228524cd0",
+    "url": "./index.html"
+  }
+];
